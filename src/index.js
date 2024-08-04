fb513649b83d4bbe608bb08dd946d583ace6dbde
yan-freshcode <freshjs.oftd@gmail.com>
2024-08-04 09:15:10 +0300

    Add getting all players in index.js with model

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const dbMongo = require('./models');
 const { players } = require('./constants/players-info');
 
 const app = express();
+const { Player, Role } = dbMongo;
 
 app.use(express.json());
 app.use(cors());
@@ -14,21 +15,30 @@ const PORT = process.env.PORT || 5000;
 
 const createPlayers = async () => {
   console.log('collection has been created')
-	const { Player } = dbMongo;
+	// const { Player } = dbMongo;
 	await Player.insertMany(players);
 };
 
 // createPlayers();
 
 const createRoles = async () => {
-  const {Role} = dbMongo;
+  // const {Role} = dbMongo;
   const roles = [
     {role: 'Admin', email: 'adm@gmail.com'},
     {role: 'Moderator', email: 'mod@gmail.com'},
     {role: 'User', email: 'user@gmail.com'},
   ]
   await Role.create(roles);
+};
+// createRoles();
+
+
+const getPlayers = async () => {
+  const gettingPlayers = await Player.find()
+  // console.log(gettingPlayers)
+  gettingPlayers.forEach(player => console.log('Player is: ', player))
 }
 
-// createRoles();
+// getPlayers()
+
 app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`));
