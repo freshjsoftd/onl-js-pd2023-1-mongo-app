b55efd44c917094accd725db9d1975c73cb60f71
yan-freshcode <freshjs.oftd@gmail.com>
2024-08-07 16:15:46 +0300

    Add getById and getVeterans for players

--- src/controllers/playersController.js
+++ src/controllers/playersController.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 
 const { Player, Role } = require('../models');
+const { ObjectId } = require('mongodb');
 
 class PlayerController {
 	async getAllPlayers(req, res, next) {
@@ -23,9 +24,61 @@ class PlayerController {
 				console.log(`Players are: ${JSON.stringify(players, null, 2)}`);
 				res.status(200).json(players);
 			} else {
-				console.log('Any players has not been found');
-				next(createError(404, 'Any players has not been found'));
+				console.log('Any players have not been found');
+				next(createError(404, 'Any players have not been found'));
 			}
+		} catch (error) {
+			console.log(error.message);
+			next(error);
+		}
+	}
+
+	async getPlayerById(req, res, next) {
+		try {
+			const { id } = req.params;
+			console.log(id);
+			const player = await Player.findById(
+				{ _id: id },
+				{
+					firstName: 1,
+					lastName: 1,
+					age: 1,
+					'address.country': 1,
+					_id: 0,
+				}
+			);
+			if (player) {
+				console.log(`Player is: ${JSON.stringify(player, null, 2)}`);
+				res.status(200).json(player);
+			}else {
+				console.log('Player has not been found');
+				next(createError(404, 'Player has not been found'));
+			}
+		} catch (error) {
+			console.log(error.message);
+			next(error);
+		}
+	}
+
+	async getVeterans(req, res, next) {
+		try {
+			const {age} = req.query;
+			console.log(age)
+			const veterans = await Player.find(
+				{ age: { $gte: age } },
+				{
+					firstName: 1,
+					lastName: 1,
+					age: 1,
+					_id: 0,
+				});
+				if (veterans) {
+					console.log(`Player is: ${JSON.stringify(veterans, null, 2)}`);
+					res.status(200).json(veterans);
+				}else {
+					console.log('Any players have not been found');
+					next(createError(404, 'Any players have not been found'));
+				}
 		} catch (error) {
 			console.log(error.message);
 			next(error);
--- src/routers/playersRouter.js
+++ src/routers/playersRouter.js
@@ -6,8 +6,16 @@ const { paginate } = require('../middlewares');
 
 const playerRouter = new Router();
 
-playerRouter
-.route('/')
+playerRouter.route('/')
 .get(paginate.paginate, playerCtrl.getAllPlayers);
+
+playerRouter.route('/age')
+.get(playerCtrl.getVeterans);
+
+
+playerRouter.route('/:id')
+.get(playerCtrl.getPlayerById);
+
+
 
 module.exports = playerRouter;
